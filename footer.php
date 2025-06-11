a64321f3cbaf3790166e5990860e272a3aa563ce
namamu <emailmu@example.com>
2025-06-11 13:47:04 +0700

    update terbaru guys

---
+++ footer.php
@@ -0,0 +1,85 @@
+        <!-- footer begin -->
+        <footer class="section-dark">
+            <div class="container">
+                <div class="row gx-5">
+                    <div class="col-lg-4 col-sm-6">
+                        <img src="images/logo.webp" class="w-150px" alt="" >
+                        <div class="spacer-20"></div>
+                        <p>We’re passionate about keeping your gadgets in top shape. Our dedicated team of experienced technicians combines technical expertise with exceptional customer service to provide reliable repairs for all your electronic devices.</p>
+
+                        <div class="social-icons mb-sm-30">
+                            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                            <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-discord"></i></a>
+                            <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+                            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-12 order-lg-1 order-sm-2">
+                        <div class="row">
+                            <div class="col-lg-6 col-sm-6">
+                                <div class="widget">
+                                    <h5>Company</h5>
+                                    <ul>                                        
+                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="services">Our Services</a></li>
+                                        <li><a href="track.html">Track My Repair</a></li>
+                                        <li><a href="book-repair.html">Book a Repair</a></li>
+                                        <li><a href="blog.html">Blog</a></li>
+                                        <li><a href="contact.html">Contact</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-6">
+                                <div class="widget">
+                                    <h5>Our Services</h5>
+                                    <ul>
+                                        <li><a href="services.html">Phone Repair</a></li>
+                                        <li><a href="services.html">Tablet Repair</a></li>
+                                        <li><a href="services.html">Computer &amp; Laptop</a></li>
+                                        <li><a href="services.html">Game Console</a></li>
+                                        <li><a href="services.html">Smartwatch Repair</a></li>
+                                        <li><a href="services.html">Software Repair</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 order-lg-2 order-sm-1">
+                        <div class="widget">
+                            <div class="fw-bold text-white"><i class="icofont-clock-time me-2 id-color-2"></i>We're Open</div>
+                            Monday - Saturday 08.00 - 18.00
+
+                            <div class="spacer-20"></div>
+
+                            <div class="fw-bold text-white"><i class="icofont-location-pin me-2 id-color-2"></i>Workshop Location</div>
+                            100 S Main St, New York, NY
+
+                            <div class="spacer-20"></div>
+
+                            <div class="fw-bold text-white"><i class="icofont-envelope me-2 id-color-2"></i>Send a Message</div>
+                            contact@gadgetfix.com                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="subfooter">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="de-flex">
+                                <div class="de-flex-col">
+                                    Copyright 2024 - GadgetFix by Designesia
+                                </div>
+                                <ul class="menu-simple">
+                                    <li><a href="#">Terms &amp; Conditions</a></li>
+                                    <li><a href="#">Privacy Policy</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- footer close -->
+    </div>
---
+++ overlay.php
@@ -0,0 +1,91 @@
+    <!-- overlay content begin -->
+    <div id="extra-wrap" class="text-light">
+        <div id="btn-close">
+            <span></span>
+            <span></span>
+        </div>
+
+        <div id="extra-content">
+            <img src="images/logo.webp" class="w-150px" alt="">
+
+            <div class="spacer-30-line"></div>
+
+            <h5 class="mb-3">Our Services</h5>
+            
+            <div class="row g-2">
+                <div class="col-lg-4 col-sm-4">
+                    <a class="p-0 py-2 text-center d-block" href="single-repair.html">
+                        <img src="images/svg-color/iphone-svgrepo-com.svg" class="w-50px mb-3" alt="">
+                        <h6 class="mb-0">Phone Repair</h6>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 col-sm-4">
+                    <a class="p-0 py-2 text-center d-block" href="single-repair.html">
+                        <img src="images/svg-color/tablet-svgrepo-com.svg" class="w-50px mb-3" alt="">
+                        <h6 class="mb-0">Tablet Repair</h6>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 col-sm-4">
+                    <a class="p-0 py-2 text-center d-block" href="single-repair.html">
+                        <img src="images/svg-color/macbook-pro-svgrepo-com.svg" class="w-50px mb-3" alt="">
+                        <h6 class="mb-0">Laptop Repair</h6>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 col-sm-4">
+                    <a class="p-0 py-2 text-center d-block" href="single-repair.html">
+                        <img src="images/svg-color/game-svgrepo-com.svg" class="w-50px mb-3" alt="">
+                        <h6 class="mb-0">Game Console</h6>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 col-sm-4">
+                    <a class="p-0 py-2 text-center d-block" href="single-repair.html">
+                        <img src="images/svg-color/smartwatch-svgrepo-com.svg" class="w-50px mb-3" alt="">
+                        <h6 class="mb-0">Smartwatch</h6>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 col-sm-4">
+                    <a class="p-0 py-2 text-center d-block" href="single-repair.html">
+                        <img src="images/svg-color/software-svgrepo-com.svg" class="w-50px mb-3" alt="">
+                        <h6 class="mb-0">Software Repair</h6>
+                    </a>
+                </div>
+            </div>
+
+
+            <div class="spacer-30-line"></div>
+
+            <h5>Contact Us</h5>
+            <div><i class="icofont-clock-time me-2 op-5"></i>Monday - Saturday 08.00 - 18.00</div>
+            <div><i class="icofont-location-pin me-2 op-5"></i>100 S Main St, New York, </div>
+            <div><i class="icofont-envelope me-2 op-5"></i>contact@gadgetfix.com</div>    
+
+            <div class="spacer-30-line"></div>
+
+            <h5>About Us</h5>
+            <p>We’re passionate about keeping your gadgets in top shape. Our dedicated team of experienced technicians combines technical expertise with exceptional customer service to provide reliable repairs for all your electronic devices.</p>
+
+            <div class="social-icons">
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+            </div>
+        </div>
+    </div>
+    <!-- overlay content end -->
+
+
+    
+    <!-- Javascript Files
+    ================================================== -->
+    <script src="js/plugins.js"></script>
+    <script src="js/designesia.js"></script>
+    <script src="js/swiper.js"></script>
+    <script src="js/custom-marquee.js"></script>
+    <script src="js/custom-swiper-1.js"></script>
